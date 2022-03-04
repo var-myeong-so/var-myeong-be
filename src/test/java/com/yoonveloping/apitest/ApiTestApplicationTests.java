edda3464aff64882441b690abb3ca29282d0bff7
Yunkeun <yunx2@naver.com>
2022-03-05 01:53:49 +0900

    feat: 프로그램 초기 설정

---
+++ src/test/java/com/yoonveloping/apitest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

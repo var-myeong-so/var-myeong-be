e6b1ab2771d5dd64c57d321bc1b20afcf9885b08
ecsimsw <ecsimsw@gmail.com>
2022-03-04 13:25:23 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/varmyeongso/varnames/VarNamesApplication.java
@@ -0,0 +1,13 @@
+package com.varmyeongso.varnames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarNamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarNamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/varmyeongso/varnames/VarNamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varmyeongso.varnames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarNamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

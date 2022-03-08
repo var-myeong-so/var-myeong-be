353eb135d35fc6d1c34117ee3c4251609ee61499
Yunkeun <yunx2@naver.com>
2022-03-09 00:00:44 +0900

    feat: gitignore 적용

--- src/main/java/com/yoonveloping/apitest/papago/Secret.java
+++
@@ -1,8 +0,0 @@
-package com.yoonveloping.apitest.papago;
-
-public class Secret {
-
-	public static final String CLIENT_ID = "NHu57TwLuoiWxpTeyNzQ"; // 애플리케이션 클라이언트 아이디값";
-	public static final String CLIENT_SECRET = "DHC2EaXWsJ"; // 애플리케이션 클라이언트 시크릿값";
-
-}

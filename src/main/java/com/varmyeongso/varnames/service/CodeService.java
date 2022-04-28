acae4426c6f0d921526ab4f67c0a7f48b4318bdd
Yunkeun <yunx2@naver.com>
2022-04-28 17:57:33 +0900

    feat: 코드 내용을 클래스명을 포함하여 10줄로 파싱

--- src/main/java/com/varmyeongso/varnames/service/CodeService.java
+++ src/main/java/com/varmyeongso/varnames/service/CodeService.java
@@ -1,10 +1,12 @@
 package com.varmyeongso.varnames.service;
 
 import com.varmyeongso.varnames.domain.Code;
+import com.varmyeongso.varnames.domain.CodeRegex;
 import com.varmyeongso.varnames.domain.CodeRepository;
 import com.varmyeongso.varnames.util.CodeFactory;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +26,8 @@ public class CodeService {
 	public List<Code> searchCodesByClassName(String keyword, String target) {
 		if (keyword.equals(KEYWORD_CLASS)) {
 			String baseName = CodeFactory.addExtension(target);
-			return codeRepository.findCodesByClassName(baseName);
+			List<Code> codesByClassName = codeRepository.findCodesByClassName(baseName);
+			return parse(codesByClassName);
 		}
 		if (keyword.equals(KEYWORD_VAR)) {
 //			return codeRepository.findCodesByVariableName(target);
@@ -37,44 +40,43 @@ public class CodeService {
 		String baseName = CodeFactory.addExtension(target);
 		List<Code> codes = codeRepository.findCodesByClassName(baseName);
 //		codes.addAll(codesByVariableName);
-		return codes;
+		return parse(codes);
 	}
 
-	private List<Code> parse(List<Code> codes, String target) {
-		codes.forEach(code -> parseCode(target, code));
-		return codes;
+	private List<Code> parse(List<Code> code) {
+		code.forEach(this::parseCode);
+		return code;
 	}
 
-	private void parseCode(String target, Code code) {
+	private void parseCode(Code code) {
 		String codeContext = code.getCode();
-		List<String> codesSplitWithNewLine = List.of(codeContext.split("\n"));
-		for (String sentence : codesSplitWithNewLine) {
-			if (parseWithIndex(target, code, codesSplitWithNewLine, sentence)) {
-				break;
-			}
-		}
+		List<String> codesByLine = List.of(codeContext.split("\n"));
+		saveParsedCode(codesByLine, code);
 	}
 
-	private boolean parseWithIndex(String target, Code code, List<String> split, String sentence) {
-		// 여기서도 결국 class에 대한 regex가 필요하겠네요
+	private void saveParsedCode(List<String> codesByLine, Code code) {
 		List<String> parsedCodes = new ArrayList<>();
-		int index;
-		if (sentence.contains(target)) {
-			index = split.indexOf(sentence);
-			if (index < 5) {
-				for (int i = 0; i < 10; i++) {
-					parsedCodes.add(split.get(i));
-				}
-				code.setCode(makeString(parsedCodes));
-				return true;
-			}
-			for (int i = index - 5; i < index + 5; i++) {
-				parsedCodes.add(split.get(i));
+		int targetIndex = calculateClassIndex(codesByLine);
+		if (targetIndex < 5) {
+			for (int i = 0; i < 10; i++) {
+				parsedCodes.add(codesByLine.get(i));
 			}
 			code.setCode(makeString(parsedCodes));
-			return true;
+			return;
 		}
-		return false;
+		for (int i = targetIndex - 5; i < targetIndex + 5; i++) {
+			parsedCodes.add(codesByLine.get(i));
+		}
+		code.setCode(makeString(parsedCodes));
+	}
+
+	private int calculateClassIndex(List<String> codesByLine) {
+		for (String line : codesByLine) {
+			if (Pattern.matches(CodeRegex.CLASS_LINE_REGEX.getRegex(), line)) {
+				return codesByLine.indexOf(line);
+			}
+		}
+		return 0;
 	}
 
 	private String makeString(List<String> strings) {

8e1aca41ad9d52f7885bf6021ce82f1f840bb19f
ecsimsw <ecsimsw@gmail.com>
2022-05-07 05:14:59 +0900

    feat : swagger 추가

--- springboot/src/main/java/com/ecsimsw/springelk/SpringElkApplication.java
+++ springboot/src/main/java/com/ecsimsw/springelk/SpringElkApplication.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.annotation.PostConstruct;
 
+@EnableWebMvc
 @SpringBootApplication
 public class SpringElkApplication {
 

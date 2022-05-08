64d969c7981220cc30ea920d942b85591ff7cf69
Yunkeun <yunx2@naver.com>
2022-05-09 03:13:22 +0900

    feat: 저장된 프로젝트에서 자바 파일 찾아 코드 읽기

--- springboot/src/main/java/com/ecsimsw/springelk/application/CodeCrawler.java
+++ springboot/src/main/java/com/ecsimsw/springelk/application/CodeCrawler.java
@@ -1,7 +1,12 @@
 package com.ecsimsw.springelk.application;
 
 import com.ecsimsw.springelk.dto.CodeFile;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,23 +14,64 @@ import java.util.List;
 @Service
 public class CodeCrawler {
 
-    private static final String CLONE_PATH = "springboot/src/main/resources/githubcodes";
-    private static Integer DIRECTORY_NUMBER = 0;
+	private static final String CLONE_PATH = "springboot/src/main/resources/githubcodes";
+	private static final String JAVA_EXTENSION = "java";
+	private static final String NEW_LINE = "\n";
+	private static Integer DIRECTORY_NUMBER = 0;
+	private final List<File> javaFiles = new ArrayList<>();
 
-    public List<CodeFile> execute(String url) throws IOException, InterruptedException {
-        saveCodes(url);
+	public List<CodeFile> execute(String url) throws IOException, InterruptedException {
+		saveCodes(url);
+		readCode();
 		return null;
-    }
+	}
 
-    private void saveCodes(String url) throws IOException, InterruptedException {
-        String command = makeCommandGitClone(url);
-        Process process = Runtime.getRuntime().exec(command);
-        process.waitFor();
-        System.out.println("exit: " + process.exitValue());
-        process.destroy();
-    }
+	private void saveCodes(String url) throws IOException, InterruptedException {
+		String command = makeCommandGitClone(url);
+		Process process = Runtime.getRuntime().exec(command);
+		process.waitFor();
+		System.out.println("exit: " + process.exitValue());
+		process.destroy();
+	}
 
-    private String makeCommandGitClone(String url) {
-        return "git clone " + url + " " + CLONE_PATH + "/" + DIRECTORY_NUMBER++;
-    }
+	private String makeCommandGitClone(String url) {
+		return "git clone " + url + " " + CLONE_PATH + "/" + DIRECTORY_NUMBER++;
+	}
+
+	private List<File> findJavaFiles(String path) {
+		final File directory = new File(path);
+		List<File> files = List.of(Objects.requireNonNull(directory.listFiles()));
+		for (File file : files) {
+			if (file.isDirectory()) {
+				findJavaFiles(file.getPath());
+			} else if (file.getName().endsWith(JAVA_EXTENSION)) {
+				javaFiles.add(file);
+			}
+		}
+		return javaFiles;
+	}
+
+	private List<String> readCode() throws IOException {
+		List<File> javaFiles = findJavaFiles(CLONE_PATH);
+		List<String> codes = new ArrayList<>();
+		for (File javaFile : javaFiles) {
+			FileReader fileReader = new FileReader(javaFile);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String codeFile = getFileContents(bufferedReader);
+			codes.add(codeFile);
+		}
+		return codes;
+	}
+
+	private String getFileContents(BufferedReader bufferedReader) throws IOException {
+		final StringBuilder fileContents = new StringBuilder();
+		while (true) {
+			String line = bufferedReader.readLine();
+			if (line == null) {
+				break;
+			}
+			fileContents.append(line).append(NEW_LINE);
+		}
+		return fileContents.toString();
+	}
 }

12ad0d198068e79df5b4fd3a5e2f03f945f04ee3
Yunkeun <yunx2@naver.com>
2022-05-09 01:30:32 +0900

    feat: git clone 구현하여 로컬에 프로젝트 저장

--- springboot/src/main/java/com/ecsimsw/springelk/application/CodeCrawler.java
+++ springboot/src/main/java/com/ecsimsw/springelk/application/CodeCrawler.java
@@ -1,6 +1,7 @@
 package com.ecsimsw.springelk.application;
 
 import com.ecsimsw.springelk.dto.CodeFile;
+import java.io.IOException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,7 +9,23 @@ import java.util.List;
 @Service
 public class CodeCrawler {
 
-    public List<CodeFile> execute(String url) {
-        return null;
+    private static final String CLONE_PATH = "springboot/src/main/resources/githubcodes";
+    private static Integer DIRECTORY_NUMBER = 0;
+
+    public List<CodeFile> execute(String url) throws IOException, InterruptedException {
+        saveCodes(url);
+		return null;
+    }
+
+    private void saveCodes(String url) throws IOException, InterruptedException {
+        String command = makeCommandGitClone(url);
+        Process process = Runtime.getRuntime().exec(command);
+        process.waitFor();
+        System.out.println("exit: " + process.exitValue());
+        process.destroy();
+    }
+
+    private String makeCommandGitClone(String url) {
+        return "git clone " + url + " " + CLONE_PATH + "/" + DIRECTORY_NUMBER++;
     }
 }

ee339f0d0dd336f65cb0d98192a924c4b78415be
Yunkeun <yunx2@naver.com>
2022-05-19 15:11:36 +0900

    fix: 허용 메서드를 GET으로 한정

--- springboot/src/main/java/com/ecsimsw/springelk/config/WebConfig.java
+++ springboot/src/main/java/com/ecsimsw/springelk/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST");
+            .allowedMethods("GET");
     }
 
     @Override
